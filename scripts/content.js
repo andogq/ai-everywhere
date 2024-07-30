e41875d33d882110f546fca4b769181eab2b188b
Tom Anderson <tom@ando.gq>
2024-07-30 22:38:49 +1000

    inital commit

---
+++ scripts/content.js
@@ -0,0 +1,44 @@
+const FLAGS = "gmi";
+const SEARCH_TERMS = ["ai", "artificial intelligence"].flatMap((term) => [
+    new RegExp(`^${term}`, FLAGS),
+    new RegExp(`(\\W)${term}(\\W)`, FLAGS),
+    new RegExp(`${term}$`, FLAGS),
+]);
+
+const REPLACEMENT_TERMS = [
+    "interns",
+    "intern",
+    "the interns",
+    "the intern",
+    "a group of interns",
+];
+
+function randomTerm() {
+    return REPLACEMENT_TERMS[
+        Math.floor(Math.random() * REPLACEMENT_TERMS.length)
+    ];
+}
+
+const walker = document.createTreeWalker(
+    document.body,
+    NodeFilter.SHOW_TEXT,
+    null,
+    false,
+);
+
+let node;
+while ((node = walker.nextNode())) {
+    node.nodeValue = SEARCH_TERMS.reduce(
+        (value, regex) =>
+            value.replaceAll(regex, (_, p1, p2) => {
+                // If this regex has capture groups, ensure they're retained in the result
+                if (typeof p1 === "string" && typeof p2 === "string") {
+                    return p1 + randomTerm() + p2;
+                }
+
+                // No capture group, just random term
+                return randomTerm();
+            }),
+        node.nodeValue,
+    );
+}
